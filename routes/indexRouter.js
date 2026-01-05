951f51e41085bbdd5a1c3f6f8a08b35bba1dec25
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-01-05 18:34:03 -0500

    feat: add example messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,21 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) => {
-  res.render("index", { message: "Message Indeכ" });
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
 module.exports = indexRouter;
