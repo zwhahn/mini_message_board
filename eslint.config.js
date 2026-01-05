59a60456bbe31850e6a8ab73552bb2c7f44c0486
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-01-05 17:26:49 -0500

    feat: configure eslint for node

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+    },
+    env: {
+      node: true,
+    },
+  },
+];
