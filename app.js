d8a2b00f050b76d4155652cd0440554a37ba91b2
zwhahn <117476820+zwhahn@users.noreply.github.com>
2026-01-05 17:54:31 -0500

    feat: install ejs

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
 const newMessageRouter = require("./routes/newMessageRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
